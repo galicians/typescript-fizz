82a6298c3a7cf1540e91211bf54fa77a9ad87f5c
galicians <galicians@gmail.com>
2020-05-09 19:47:55 +0100

    1 file modified

--- index.ts
+++ index.ts
@@ -3,21 +3,21 @@ import './style.css';
 
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starterr</h1>`;
+appDiv.innerHTML = `<h1>TypeScript Fizzbuzz</h1>`;
 
 const fizzbuzz = (limit: number):void => {
   let num = 1; 
   console.log('test')
   
   while (num < limit) {
-    appDiv.innerHTML += num 
+    appDiv.innerHTML += num
     if (num % 3 == 0) {
       if (num % 5 == 0) appDiv.innerHTML += 'fizzbuzz'
       else appDiv.innerHTML += 'fizz'
     } 
-    if (num % 5 == 0) appDiv.innerHTML += 'buzz'
-    num += 1
+    else if (num % 5 == 0) appDiv.innerHTML += 'buzz'
 
+    num += 1
     appDiv.innerHTML +=  '<br>'
   }
 
