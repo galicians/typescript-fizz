67bd6427ecf6928bbfe4534616015e109d517c0a
galicians <galicians@gmail.com>
2020-05-09 18:52:45 +0100

    1 file modified

--- index.ts
+++ index.ts
@@ -6,17 +6,20 @@ const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starterr</h1>`;
 
 const fizzbuzz = (limit: number):void => {
-  let num = 0; 
+  let num = 1; 
   console.log('test')
-  appDiv.innerHTML += 1
-  // while (num < limit) {
-  //   if (num % 3 == 0) {
-  //     if (num % 5 == 0) console.log('fizzbuzz')
-  //     else console.log('fizz')
-  //   } 
-  //   if (num % 5 == 0) console.log('buzz')
-  //   num += num
-  // }
+  
+  while (num < limit) {
+    appDiv.innerHTML += num 
+    if (num % 3 == 0) {
+      if (num % 5 == 0) appDiv.innerHTML += 'fizzbuzz'
+      else appDiv.innerHTML += 'fizz'
+    } 
+    if (num % 5 == 0) appDiv.innerHTML += 'buzz'
+    num += 1
+
+    appDiv.innerHTML +=  '<br>'
+  }
 
 };
 
