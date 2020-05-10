6d11d309c1161b233aa0b44ff493a3ff32eaa7d6
galicians <galicians@gmail.com>
2020-05-10 12:01:17 +0100

    1 file modified

--- index.ts
+++ index.ts
@@ -7,24 +7,22 @@ appDiv.innerHTML = `<h1>TypeScript Fizzbuzz</h1>`;
 
 const fizzbuzz = (limit: number):void => {
   let num = 1; 
-  console.log('test')
-  
+
   while (num < limit) {
-    // appDiv.innerHTML += num
 
-  if (num % 3 == 0) {
-    if (num % 5 ==0 ) {
-      appDiv.innerHTML += 'fizzbuzz'
-    } else appDiv.innerHTML += 'fizz'
-
-} else if (num % 5 == 0) appDiv.innerHTML += 'buzz'
-else appDiv.innerHTML += num
-
+    if (num % 3 == 0) {
+      if (num % 5 ==0 ) {
+        appDiv.innerHTML += 'fizzbuzz'
+      } else appDiv.innerHTML += 'fizz'
+    } 
+    else if (num % 5 == 0) appDiv.innerHTML += 'buzz'
+    else appDiv.innerHTML += num
 
     num += 1
     appDiv.innerHTML +=  '<br>'
-  }
 
+  }
+  
 };
 
 fizzbuzz(100)
